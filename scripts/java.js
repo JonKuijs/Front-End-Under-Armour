ec22d6d7699dbc8078f0d16f5a4252afb673c0b4
JonKuijs <74182293+JonKuijs@users.noreply.github.com>
2021-01-27 10:27:42 +0100

    Add files via upload

--- scripts/java.js
+++ scripts/java.js
@@ -1,3 +1,7 @@
+// met hulp van marijn heb ik hier het hamburger menu dat uitklapt het komt er op Neer Dat als je op hamburger klikt dat de functie
+// uit klap wordt gestart in css die de input weer naar beneden moet brengen.
+
+
 var hamburger = document.getElementById('hamburger');
 var menu = document.getElementById('menu');
 
@@ -7,13 +11,24 @@ function uitklappen() {
 
 hamburger.addEventListener('click', uitklappen);
 
-
+// Dit is het zelfde als het hamburger menu maar met een extratje er zijn meerderen items die naar benenden moet dus meer
+// variabelen die uit eindelijk getoggled moeten worden. Als laatsten is er ook nog een extra event listener toe gevoegd
+// om zo op kruisje het weer weg te kunnen klikken.
 
 var vergrootglas = document.getElementById('vergrootglas');
 var zoek = document.getElementById('zoek');
+var wegklik = document.getElementById('wegklik');
+var vergrootglas2 = document.getElementById('vergrootglas2')
+
 
 function opzoeken() {
   zoek.classList.toggle('zoeken');
+  wegklik.classList.toggle('wegklikken');
+  vergrootglas2.classList.toggle('decoratiezoek')
+
+
 };
 
+
 vergrootglas.addEventListener('click', opzoeken);
+wegklik.addEventListener('click', opzoeken);
